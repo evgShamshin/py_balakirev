fc4c04eb15204e6bece69fcaa284019d067282e8
evg_shamshin <i@eshamshin.ru>
2024-09-05 23:19:00 +0300

    24.09.05

--- 7_functions/ 7-7_recursive functions/7.7.7.py
+++ 7_functions/ 7-7_recursive functions/7.7.7.py
@@ -1,5 +1,11 @@
-def get_path(n, step = 0):
-    if step + 1 < n:
-        step += 1
-    elif step + 2 < n:
-        step += 2
+def get_path(n, count=0):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n > 0:
+        count += get_path(n - 1, count) + get_path(n - 2, count)
+    return count
+
+
+print(get_path(int(input())))
