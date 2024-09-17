ac0af8d9da9159f4063a6998d51e57a8794b7342
evg_shamshin <i@eshamshin.ru>
2024-09-17 22:40:01 +0300

    24.09.17

--- 8_modules and packages/8-2_natural modules/8-2-3/libs/8.2.3.py
+++ 8_modules and packages/8-2_natural modules/8-2-3/libs/8.2.3.py
@@ -1,5 +1,4 @@
 import sys
 sys.path.append(r"C:\Users\evg_shamshin\Documents\py_projects\py_balakirev\8_ modules and packages\8-2_natural modules\8-2-3")
-import panda
 
 print(sys.path)
---
+++ 8_modules and packages/8-3_packages python/8-3-1/panda_pack/__init__.py
@@ -0,0 +1,2 @@
+import sys
+sys.path.append(r"C:\Users\evg_shamshin\Documents\py_projects\py_balakirev\8_modules and packages\8-3_packages python\8-3-1")
