5a88fa28542ee408941e50f7ff0f519926227fe1
evg_shamshin <i@eshamshin.ru>
2024-09-30 22:15:45 +0300

    24.09.30
    Signed-off-by: evg_shamshin <i@eshamshin.ru>

--- 10_final/10-5_match and case-list and tuple/10.5.5.py
+++ 10_final/10-5_match and case-list and tuple/10.5.5.py
@@ -2,13 +2,13 @@ t = (int, str, str, float, int)
 book = [t[i](x) if t[i] != str else x.strip() for i, x in enumerate(input().split(","))]
 
 match book:
-    case num, author, book:
+    case num, author, book if len(author) > 5 and len(book) > 9:
         print("Yes")
-    case num, author, book, cost:
+    case num, author, book, cost if len(author) > 5 and cost > 0:
         print("Yes")
-    case num, author, book, year:
+    case num, author, book, year if year > 2019:
         print("Yes")
-    case num, author, book, cost, year:
+    case num, author, book, cost, year if cost > 0 and year > 2019:
         print("Yes")
     case _:
         print("No")
